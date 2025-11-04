83465eef73d4fda0e787b7f4f10eec5a5601baeb
Danielle Davis <113556576+DevDaniP@users.noreply.github.com>
2025-11-04 18:39:25 +0000

    day 10

--- day-010/calculatorImproved.py
+++ day-010/calculatorImproved.py
@@ -1,3 +1,4 @@
+from art import logo 
 def add(n1, n2):
     return n1 + n2
 
@@ -17,12 +18,13 @@ arithOpp = {
     '*': multiply 
 }
 
+print(logo)
 while True:
-    num1 = float(input("Input :\n"))
+    num1 = float(input("Enter the first number you want to calculate:\n"))
     
     while True:
-        opp = input("Operator (+, -, /, *):\n")
-        num2 = float(input("Second number:\n"))
+        opp = input("Enter the Operator you wish to use (+, -, /, *):\n")
+        num2 = float(input("Enter the second number you wish to calculate:\n"))
         num1 = arithOpp[opp](num1, num2)
         print(f"Result: {num1}")
         
